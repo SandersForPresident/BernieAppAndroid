7afaff320b974c5dd1bdc80dfca39165d3d065be
Steve <steve.vernyi@gmail.com>
2015-07-15 03:07:20 -0400

    dynamic map & listview base is a go

--- app/src/main/java/com/spielpark/steve/bernieapp/fragments/ConnectFragment.java
+++ app/src/main/java/com/spielpark/steve/bernieapp/fragments/ConnectFragment.java
@@ -1,68 +1,119 @@
 package com.spielpark.steve.bernieapp.fragments;
 
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.spielpark.steve.bernieapp.R;
+import com.spielpark.steve.bernieapp.actMainPage;
+import com.spielpark.steve.bernieapp.tasks.ConnectTask;
+import com.spielpark.steve.bernieapp.wrappers.Event;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConnectFragment extends Fragment {
-
-
-    public ConnectFragment() {
-        // Required empty public constructor
+    private static GoogleMap map;
+    private int mZip = 0;
+    private int mRadius = 50;
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        ((Button) getView().findViewById(R.id.c_btnRadius)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                android.support.v7.app.AlertDialog.Builder bld = new android.support.v7.app.AlertDialog.Builder(getActivity());
+                bld.setTitle("Pick a Radius");
+                bld.setSingleChoiceItems(R.array.radius_choices, 1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        setRadius(i);
+                        dialogInterface.dismiss();
+                    }
+                });
+                bld.create().show();
+            }
+        });
+        ((Button) getView().findViewById(R.id.c_btnGo)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTask();
+            }
+        });
+        setUpMap();
+        super.onViewCreated(view, savedInstanceState);
     }
 
-
-
-    public static Bitmap getGoogleMapThumbnail(double lati, double longi){
-        String URL = "http://maps.google.com/maps/api/staticmap?center=" +lati + "," + longi + "&zoom=15&size=200x200&sensor=false";
-        Bitmap bmp = null;
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpGet request = new HttpGet(URL);
-
-        InputStream in = null;
-        try {
-            in = httpclient.execute(request).getEntity().getContent();
-            bmp = BitmapFactory.decodeStream(in);
-            in.close();
-        } catch (IllegalStateException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return bmp;
+    private void startTask() {
+        ListView list = (ListView) getView().findViewById(R.id.c_listEvents);
+        ProgressBar prog = (ProgressBar) getView().findViewById(R.id.c_progress);
+        mZip = Integer.parseInt(((EditText) getView().findViewById(R.id.c_edtZip)).getText().toString());
+        new ConnectTask(getActivity(), list, prog, getView().findViewById(R.id.c_mapContainer), mZip, mRadius).execute();
     }
 
+    private void setRadius(int m) {
+        mRadius = m++ < 4 ? m*25 : ((m-2)*50);
+        ((Button) getView().findViewById(R.id.c_btnRadius)).setText(mRadius + " miles");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.frag_connect, container, false);
     }
 
+    private void setUpMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.c_map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                ConnectFragment.this.map = googleMap;
+            }
+        });
+    }
 
+    public static void setMarkers() {
+        ArrayList<Event> events = ConnectTask.getEvents();
+        LatLngBounds.Builder bld = new LatLngBounds.Builder();
+        List<Marker> markers = new ArrayList<>(events.size());
+        LatLng pos;
+        for (Event e : ConnectTask.getEvents()) {
+            pos = new LatLng(e.getLatitude(), e.getLongitude());
+            markers.add(map.addMarker(new MarkerOptions()
+                    .position(pos)
+                    .title(e.getName())));
+            bld.include(pos);
+        }
+        LatLngBounds bounds = bld.build();
+        map.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 0));
+        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Log.d("Marker Clicked", marker.getId());
+                return true;
+            }
+        });
+    }
 }
