52f490d9a2125b0350f367f3bcdf8042a6777645
Steve <steve.vernyi@gmail.com>
2015-08-17 10:30:58 -0400

    used by ImgTxtAdapter

---
+++ app/src/main/java/com/spielpark/steve/bernieapp/wrappers/ImgTxtItem.java
@@ -0,0 +1,17 @@
+package com.spielpark.steve.bernieapp.wrappers;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Steve on 8/17/2015.
+ */
+public interface ImgTxtItem {
+    public String getTxt();
+
+    public void setTxt(String txt);
+
+    public Bitmap getImg();
+
+    public void setImg(Bitmap img);
+
+}
