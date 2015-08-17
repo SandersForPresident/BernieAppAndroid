6635033ffc60ba0b49e90b2a078c8e279dd062b8
Steve <steve.vernyi@gmail.com>
2015-08-17 10:29:22 -0400

    updated to NewsArticle object

--- app/src/main/java/com/spielpark/steve/bernieapp/fragments/NewsFragment.java
+++ app/src/main/java/com/spielpark/steve/bernieapp/fragments/NewsFragment.java
@@ -4,7 +4,6 @@ package com.spielpark.steve.bernieapp.fragments;
  * Created by Steve on 7/9/2015.
  */
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -18,7 +17,6 @@ import android.widget.ProgressBar;
 import com.spielpark.steve.bernieapp.R;
 import com.spielpark.steve.bernieapp.actMainPage;
 import com.spielpark.steve.bernieapp.tasks.NewsTask;
-import com.spielpark.steve.bernieapp.wrappers.Event;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -36,14 +34,14 @@ public class NewsFragment extends Fragment {
         }
     }
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         final ListView newsList = (ListView) view.findViewById(R.id.listNews);
-        new NewsTask(getActivity(), newsList, (ProgressBar) view.findViewById(R.id.progressBar)).execute();
+        new NewsTask(getActivity(), newsList, (ProgressBar) view.findViewById(R.id.progressBar), true).execute();
         newsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ((actMainPage)getActivity()).loadEvent(NewsTask.getEvent(position));
+                ((actMainPage) getActivity()).loadEvent(NewsTask.getArticle(position));
             }
         });
     }
