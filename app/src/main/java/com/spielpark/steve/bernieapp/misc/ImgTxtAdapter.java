248c7dd7e4a414ff6b71d577bd1cef8d62923b86
Steve <steve.vernyi@gmail.com>
2015-08-23 00:54:04 -0400

    removed unnecessary imports

--- app/src/main/java/com/spielpark/steve/bernieapp/misc/ImgTxtAdapter.java
+++ app/src/main/java/com/spielpark/steve/bernieapp/misc/ImgTxtAdapter.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.spielpark.steve.bernieapp.R;
-import com.spielpark.steve.bernieapp.wrappers.Event;
 import com.spielpark.steve.bernieapp.wrappers.ImgTxtItem;
-import com.spielpark.steve.bernieapp.wrappers.Issue;
-import com.spielpark.steve.bernieapp.wrappers.NewsArticle;
 
 import java.util.List;
 
