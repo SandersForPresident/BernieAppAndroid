ecbc54b297063ef9e69dd7f9592c4d08218794c8
Steve <steve.vernyi@gmail.com>
2015-09-30 00:23:18 -0400

    using #ES-4BS & Picasso

--- app/src/main/java/com/spielpark/steve/bernieapp/tasks/IssuesTask.java
+++ app/src/main/java/com/spielpark/steve/bernieapp/tasks/IssuesTask.java
@@ -18,6 +18,7 @@ import com.spielpark.steve.bernieapp.misc.Util;
 import com.spielpark.steve.bernieapp.wrappers.Event;
 import com.spielpark.steve.bernieapp.wrappers.ImgTxtItem;
 import com.spielpark.steve.bernieapp.wrappers.Issue;
+import com.spielpark.steve.bernieapp.wrappers.NewsArticle;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -33,6 +34,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by Steve on 7/9/2015.
@@ -48,30 +50,20 @@ public class IssuesTask extends AsyncTask {
         this.ctx = ctx;
         this.progressBar = progressBar;
     }
-    public static void clear() {
-        issues = null;
-        ctx = null;
-        list = null;
-        progressBar = null;
-        vidLinks = null;
-    }
+
     public static Issue getIssue(int position) {
         return issues.get(position);
     }
 
     private String getHTMLForTitle(Issue i) {
-        SimpleDateFormat ft = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.US);
-        String time;
-        try {
-            final Date dateObj = ft.parse(i.getPubDate());
-            time = new SimpleDateFormat("EEE, d MMM yyyy").format(dateObj);
-        } catch (ParseException | NullPointerException e) {
-            e.printStackTrace();
-            time = " date not available.";
-        }
         StringBuilder bld = new StringBuilder();
-        bld.append("<big>").append(i.getTitle()).append("</big><br>");
-        bld.append("<font color=\"#FF2222\">Published on ").append(time).append("</font>");
+        String title = i.getTitle();
+        if (title.length() > 40) {
+            title = title.substring(0, 40);
+            title = title.substring(0, Math.min(title.length(), title.lastIndexOf(' '))).concat("...");
+        }
+        bld.append("<big>").append(title).append("</big><br>");
+        bld.append("<font color=\"#FF2222\">").append(i.getPubDate());
         return bld.toString();
     }
 
@@ -80,43 +72,141 @@ public class IssuesTask extends AsyncTask {
         super.onPostExecute(o);
         for (Issue i : issues) {
             i.setVideo(vidLinks.get(i.getTitle().toLowerCase()));
-            Log.d("Success!", i.getTitle() + "..." + i.getVideo());
+            i.setImgSrc("https://img.youtube.com/vi/" + i.getVideo() + "/default.jpg");
         }
-        new FetchThumbsTask().execute();
-        ImgTxtAdapter adapter = new ImgTxtAdapter(ctx, R.layout.list_news_item, issues);
-        list.setAdapter(adapter);
+        ((ImgTxtAdapter) list.getAdapter()).notifyDataSetChanged();
         list.setVisibility(View.VISIBLE);
         progressBar.setVisibility(View.GONE);
     }
 
     @Override
-    protected Object doInBackground(Object[] params) {
-        retrieveLinks();
+    protected void onPreExecute() {
+        super.onPreExecute();
         issues = new ArrayList<>();
-        BufferedReader xml = null;
-        try {
-            URL url = new URL("https://berniesanders.com/issues/feed/");
-            xml = new BufferedReader(new InputStreamReader(url.openStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (xml == null) {
-            Issue i = new Issue();
-            i.setHtmlTitle("Unable to Load News");
-            i.setDesc("Check your internet connection?");
-            issues.add(i);
-            return null;
-        }
-        XmlPullParser xmlReader = Xml.newPullParser();
-        try {
-            xmlReader.setInput(xml);
-            readXml(xmlReader);
-        } catch (XmlPullParserException | IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        ImgTxtAdapter adapter = new ImgTxtAdapter(ctx, R.layout.list_news_item, issues);
+        list.setAdapter(adapter);
     }
 
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+        ((ImgTxtAdapter) list.getAdapter()).notifyDataSetChanged();
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        retrieveLinks();
+        for (Map.Entry<String, String> e : vidLinks.entrySet()) {
+            Log.d("K/V: ", e.getKey() + ".." + e.getValue());
+        }
+            BufferedReader in = null;
+            try {
+                URL url = new URL("https://search.berniesanders.tech/articles_en/berniesanders_com/_search?q=article_type%3A%28Issues%29&sort=created_at:desc&size=20");
+                Log.d("URL", url.toString());
+                in = new BufferedReader(new InputStreamReader(url.openStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (in == null) {
+                Log.d("reader null", "no events, null reader,");
+                Issue i = new Issue();
+                i.setHtmlTitle("Unable to Load News");
+                i.setDesc("Check your internet connection?");
+                issues.add(i);
+                return null;
+            }
+            JsonReader reader = new JsonReader(in);
+            try {
+                readObjects(reader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+    private void readObjects(JsonReader reader) throws IOException {
+        Issue i = new Issue();
+        while (reader.hasNext()) {
+            if (isCancelled()) {
+                return;
+            }
+            if (reader.peek() == JsonToken.BEGIN_OBJECT) {
+                reader.beginObject();
+            }
+            if (reader.peek() == JsonToken.BEGIN_ARRAY) {
+                reader.beginArray();
+            }
+            if (reader.peek() == JsonToken.END_ARRAY) {
+                reader.endArray();
+            }
+            String next = reader.nextName();
+            switch(next.toLowerCase().trim()) {
+                case "hits" : {
+                    if (reader.peek() == JsonToken.BEGIN_ARRAY) {
+                        reader.beginArray();
+                    } else {
+                        reader.beginObject();
+                    }
+                    break;
+                }
+                case "_source" : {
+                    reader.beginObject();
+                    break;
+                }
+                case "title" : {
+                    i.setTitle(reader.nextString());
+                    break;
+                }
+                case "url" : {
+                    i.setUrl(reader.nextString());
+                    break;
+                }
+                case "inserted_at" : {
+                    i.setPubDate(reader.nextString());
+                    break;
+                }
+                case "body" : {
+                    i.setDesc(reader.nextString());
+                    break;
+                }
+                case "_id" : {
+                    reader.skipValue();
+                    if (reader.peek() == JsonToken.END_OBJECT) {
+                        reader.endObject();
+                    }
+                    break;
+                }
+                default: {
+                    if (reader.peek() != JsonToken.END_OBJECT) {
+                        reader.skipValue();
+                    }
+                    if (reader.peek() == JsonToken.END_OBJECT) {
+                        if (i.getTitle() != null) {
+                            Log.d("New NewsArticle: ", i.getTitle());
+                            formatDate(i);
+                            i.setTxt(getHTMLForTitle(i));
+                            issues.add(i);
+                            i = new Issue();
+                            publishProgress();
+                        }
+                        reader.endObject();
+                    }
+                }
+            }
+        }
+        reader.close();
+    }
+
+    private void formatDate(Issue i) {
+        SimpleDateFormat ft;
+        ft = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS", Locale.US);
+        try {
+            Date date = ft.parse(i.getPubDate());
+            i.setPubDate(new SimpleDateFormat("MMMM d, yyyy").format(date));
+        } catch (ParseException e1) {
+            e1.printStackTrace();
+        }
+    }
     private void retrieveLinks() {
         BufferedReader in = null;
         vidLinks = new HashMap<>();
@@ -131,13 +221,13 @@ public class IssuesTask extends AsyncTask {
         }
         JsonReader reader = new JsonReader(in);
         try {
-            readObjects(reader);
+            readReddit(reader);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void readObjects(JsonReader reader) throws IOException {
+    private void readReddit(JsonReader reader) throws IOException {
         Log.d("JsonReader", "Beginning parsing");
         String title = "";
         String link = "";
@@ -157,7 +247,7 @@ public class IssuesTask extends AsyncTask {
                     break;
                 }
                 case "title" : {
-                    title = reader.nextString();
+                    title = reader.nextString().replaceAll("&amp;", "&");
                     break;
                 }
                 case "url" : {
@@ -178,61 +268,4 @@ public class IssuesTask extends AsyncTask {
         }
         reader.close();
     }
-
-    private void readXml(XmlPullParser in) throws XmlPullParserException, IOException {
-        int type = in.getEventType();
-        while (type != XmlPullParser.END_DOCUMENT) {
-            if (type == XmlPullParser.START_TAG && in.getName().equals("item")) {
-                readItem(in);
-            }
-            type = in.next();
-        }
-    }
-
-    private void readItem(XmlPullParser in) throws XmlPullParserException, IOException {
-        Issue i = new Issue();
-        int type = in.next();
-        while (!(type == XmlPullParser.END_TAG && in.getName().equals("item"))) {
-            if (type == XmlPullParser.START_TAG) {
-                String name = in.getName();
-                if (name.equals("title")) {
-                    i.setTitle(in.nextText());
-                } else if (name.equals("link")) {
-                    i.setUrl(in.getText());
-                } else if (name.equals("pubDate")) {
-                    String t = (in.nextText());
-                    i.setPubDate(t);
-                    i.setHtmlTitle(getHTMLForTitle(i));
-                } else if (name.equals("description")) {
-                    i.setDesc(in.nextText());
-                }
-            }
-            type = in.next();
-        }
-        issues.add(i);
-    }
-
-    private class FetchThumbsTask extends AsyncTask {
-
-        @Override
-        protected Object doInBackground(Object[] objects) {
-            StringBuilder bld = new StringBuilder("https://img.youtube.com/vi/");
-            for (Issue i : issues) {
-                if (i.getVideo() == null) {
-                    continue;
-                }
-                bld.append(i.getVideo());
-                bld.append("/default.jpg");
-                i.setThumb(Util.getBitmapFromURL(bld.toString()));
-                bld = new StringBuilder("http://img.youtube.com/vi/");
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Object o) {
-            ((ImgTxtAdapter) list.getAdapter()).notifyDataSetChanged();
-        }
-    }
-
 }
