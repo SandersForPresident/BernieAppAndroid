26aa7f18cb676091ce0e2cc4fdd18deb58a18db3
pat <patrick.dattilio@gmail.com>
2015-10-21 19:09:25 -0400

    SingleIssueFragment, use newInstance and fragment.setArgument to allow the fragment to recreate itself if it has been destroyed in the background. Made Issue parcelable for use in SingleIssueFragment

--- app/src/main/java/com/spielpark/steve/bernieapp/fragments/SingleIssueFragment.java
+++ app/src/main/java/com/spielpark/steve/bernieapp/fragments/SingleIssueFragment.java
@@ -2,9 +2,7 @@ package com.spielpark.steve.bernieapp.fragments;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
@@ -12,49 +10,61 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.spielpark.steve.bernieapp.R;
 import com.spielpark.steve.bernieapp.wrappers.Issue;
 
 /**
- * A simple {@link Fragment} subclass.
+ * A Fragment to display an Issue.
  */
 public class SingleIssueFragment extends Fragment {
-  private static Issue mIssue;
+  public static final String ISSUE = "ISSUE";
+  @Bind(R.id.i_txtTitle) TextView title;
+  @Bind(R.id.i_txtDate) TextView date;
+  @Bind(R.id.i_txtDesc) TextView description;
+  @Bind(R.id.i_video) WebView webView;
+  private Issue issue;
 
-  public static SingleIssueFragment newInstance(Issue i) {
-    mIssue = i;
-    return new SingleIssueFragment();
-  }
-
-  @Override public void onPause() {
-    super.onPause();
-    ((WebView) getView().findViewById(R.id.i_video)).onPause();
+  public static SingleIssueFragment newInstance(Issue issue) {
+    SingleIssueFragment fragment = new SingleIssueFragment();
+    Bundle args = new Bundle();
+    args.putParcelable(ISSUE, issue);
+    fragment.setArguments(args);
+    return fragment;
   }
 
   @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-  }
-
-  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    if (mIssue == null) {
-      //User exited the app and returned to it, but android cleared some stuff from memory...
-      getActivity().getSupportFragmentManager()
-          .popBackStack("base", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+    Bundle args = getArguments();
+    if (args != null) {
+      issue = args.getParcelable(ISSUE);
     }
-    final View root = getView();
-    ((TextView) root.findViewById(R.id.i_txtTitle)).setText(mIssue.getTitle());
-    ((TextView) root.findViewById(R.id.i_txtTitle)).setShadowLayer(13, 0, 0, Color.BLACK);
-    ((TextView) root.findViewById(R.id.i_txtDate)).setText("Published " + mIssue.getPubDate());
-    ((TextView) root.findViewById(R.id.i_txtDesc)).setText(Html.fromHtml(mIssue.getDesc()));
-    ((TextView) root.findViewById(R.id.i_txtDesc)).setMovementMethod(new LinkMovementMethod());
-    ((WebView) root.findViewById(R.id.i_video)).getSettings().setJavaScriptEnabled(true);
-    ((WebView) root.findViewById(R.id.i_video)).loadData(mIssue.getEmbedURL(getActivity()),
-        "text/Html", "UTF-8");
   }
 
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.frag_single_issue, container, false);
+    View view = inflater.inflate(R.layout.frag_single_issue, container, false);
+    ButterKnife.bind(this, view);
+
+    title.setText(issue.getTitle());
+    title.setShadowLayer(13, 0, 0, Color.BLACK);
+    date.setText("Published " + issue.getPubDate());
+    description.setText(Html.fromHtml(issue.getDesc()));
+    description.setMovementMethod(new LinkMovementMethod());
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.loadData(issue.getEmbedURL(getActivity()), "text/Html", "UTF-8");
+
+    return view;
+  }
+
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+    ButterKnife.unbind(this);
+  }
+
+  @Override public void onPause() {
+    super.onPause();
+    webView.onPause();
   }
 }
