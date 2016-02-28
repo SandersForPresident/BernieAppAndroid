ca5be60222308d7199276eb407831e93c1886450
Steve <steve.vernyi@gmail.com>
2016-02-28 17:00:59 -0500

    removes unnecessary code

--- app/src/main/java/com/spielpark/steve/bernieapp/fragments/BottomNavFragment.java
+++ app/src/main/java/com/spielpark/steve/bernieapp/fragments/BottomNavFragment.java
@@ -26,11 +26,6 @@ public class BottomNavFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
