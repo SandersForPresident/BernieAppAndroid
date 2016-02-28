188039ababf420f8c644e5d0791d0d3abed603c8
Steve <steve.vernyi@gmail.com>
2016-02-28 17:03:14 -0500

    adds share capabilities

--- app/src/main/java/com/spielpark/steve/bernieapp/fragments/SingleIssueFragment.java
+++ app/src/main/java/com/spielpark/steve/bernieapp/fragments/SingleIssueFragment.java
@@ -1,16 +1,21 @@
 package com.spielpark.steve.bernieapp.fragments;
 
-
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.TextView;
 
@@ -21,6 +26,7 @@ import com.spielpark.steve.bernieapp.wrappers.Issue;
  * A simple {@link Fragment} subclass.
  */
 public class SingleIssueFragment extends Fragment {
+    private ShareActionProvider shareActionProvider;
     private static Issue mIssue;
 
     public static SingleIssueFragment newInstance(Issue i) {
@@ -40,7 +46,7 @@ public class SingleIssueFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         if (mIssue == null) {
             //User exited the app and returned to it, but android cleared some stuff from memory...
@@ -52,14 +58,43 @@ public class SingleIssueFragment extends Fragment {
         ((TextView) root.findViewById(R.id.i_txtDate)).setText("Published " + mIssue.getPubDate());
         ((TextView) root.findViewById(R.id.i_txtDesc)).setText(Html.fromHtml(mIssue.getDesc()));
         ((TextView) root.findViewById(R.id.i_txtDesc)).setMovementMethod(new LinkMovementMethod());
-        ((WebView) root.findViewById(R.id.i_video)).getSettings().setJavaScriptEnabled(true);
-        ((WebView) root.findViewById(R.id.i_video)).loadData(mIssue.getEmbedURL(getActivity()), "text/Html", "UTF-8");
+        WebView wv = ((WebView) root.findViewById(R.id.i_video));
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadDataWithBaseURL(null, mIssue.getEmbedURL(getActivity()), "text/Html", "UTF-8", null);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.share_menu, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        shareActionProvider.setShareIntent(getShareIntent());
+        shareActionProvider.setOnShareTargetSelectedListener(new ShareActionProvider.OnShareTargetSelectedListener() {
+            @Override
+            public boolean onShareTargetSelected(ShareActionProvider source, Intent intent) {
+                getActivity().onBackPressed();
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private Intent getShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        StringBuilder text = new StringBuilder("Hey, here's Bernie Sanders' official stance on this issue -- \n");
+        text.append(mIssue.getTitle());
+        text.append("\n\nRead more here: " + mIssue.getUrl());
+        text.append("\n\nSent from Bernie Sanders for President 2016 Android Application");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text.toString());
+        return shareIntent;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.frag_single_issue, container, false);
     }
 
--- app/src/main/java/com/spielpark/steve/bernieapp/fragments/SingleNewsFragment.java
+++ app/src/main/java/com/spielpark/steve/bernieapp/fragments/SingleNewsFragment.java
@@ -5,9 +5,15 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -20,6 +26,7 @@ import com.spielpark.steve.bernieapp.wrappers.NewsArticle;
 public class SingleNewsFragment extends Fragment {
     private static NewsArticle mEvent;
     private static SingleNewsFragment mInstance;
+    private ShareActionProvider shareActionProvider;
 
     public static SingleNewsFragment getInstance(NewsArticle e) {
         mEvent = e;
@@ -52,14 +59,36 @@ public class SingleNewsFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.share_menu, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        shareActionProvider.setShareIntent(getShareIntent());
+        shareActionProvider.setOnShareTargetSelectedListener(new ShareActionProvider.OnShareTargetSelectedListener() {
+            @Override
+            public boolean onShareTargetSelected(ShareActionProvider source, Intent intent) {
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private Intent getShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        StringBuilder text = new StringBuilder("Hey, check out this article on Bernie Sanders!\n");
+        text.append(mEvent.getTitle());
+        text.append("\n\nRead more here: " + mEvent.getUrl());
+        text.append("\n\nSent from Bernie Sanders for President 2016 Android Application");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text.toString());
+        return shareIntent;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.frag_event, container, false);
     }
 }
